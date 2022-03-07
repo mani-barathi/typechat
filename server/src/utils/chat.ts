f4813b0caf7ea753ae2744990e9acf94af0a57a4
manibarathi <mani-barathi@users.noreply.github.com>
2022-03-07 23:18:20 +0530

    fetcing direct and group chats is appropriate order

--- server/src/utils/chat.ts
+++ server/src/utils/chat.ts
@@ -1,3 +1,5 @@
+import { Chat } from "../types";
+
 export const getPrivateChatRoomIds = (
   senderName: string,
   receiverName: string
@@ -5,4 +7,36 @@ export const getPrivateChatRoomIds = (
   const receiverRoomId = `${receiverName}-${senderName}`;
   const senderRoomId = `${senderName}-${receiverName}`;
   return { receiverRoomId, senderRoomId };
+};
+
+export const mergeChatArrays = (
+  directChats: Chat[],
+  groupChats: Chat[]
+): Chat[] => {
+  const allChats = [];
+  let i = 0,
+    j = 0;
+  let arr1Len = directChats.length;
+  let arr2Len = groupChats.length;
+
+  while (i < arr1Len && j < arr2Len) {
+    if (directChats[i].createdAt! < groupChats[j].createdAt!) {
+      allChats.push(groupChats[j]);
+      j++;
+    } else {
+      allChats.push(directChats[i]);
+      i++;
+    }
+  }
+
+  while (i < arr1Len) {
+    allChats.push(directChats[i]);
+    i++;
+  }
+
+  while (j < arr2Len) {
+    allChats.push(groupChats[j]);
+    j++;
+  }
+  return allChats;
 };
